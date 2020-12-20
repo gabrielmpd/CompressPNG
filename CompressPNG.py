08b9bb143ce2aa19f4b3a64b3d533e62fb5e6ef1
gabrielmpd <gaabriel.mpd@gmail.com>
2020-12-20 19:51:11 -0300

    Update CompressPNG.py

--- CompressPNG.py
+++ CompressPNG.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 #funçao para verificar se os arquivos do diretório sao imagens
 def eh_imagem(nome_arquivo):
-    if nome_arquivo.endswith('png') or nome_arquivo.endswith('jpg'):
+    if nome_arquivo.endswith('png'):
         return True
     return False
 
@@ -11,7 +11,7 @@ def reduzir_tamanho_imagens(input_dir, output_dir, ext='.jpg'):
     lista_de_arquivos = [nome for nome in os.listdir(input_dir) if eh_imagem(nome)]
     for nome in lista_de_arquivos:
         imagem = Image.open(os.path.join(input_dir, nome)).convert('RGB')
-        redimensionada = imagem.resize((1280, 720)) #resize sem mudar a resolução
+        redimensionada = imagem.resize((1280, 720))
         nome_sem_ext = os.path.splitext(nome)[0]
         redimensionada.save(os.path.join(output_dir, nome_sem_ext + ext))
 
